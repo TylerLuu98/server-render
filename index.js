305fe3d15cffa518101e814086b36fd64f980993
thanhtuvietnam <luuthanhtung555@gmail.com>
2025-02-09 00:13:14 +0700

    fix cors

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Tăng thời gian chờ kết nối lên 30 giây
   })
   .then(() => {
     console.log("Connected to MongoDB Atlas");
